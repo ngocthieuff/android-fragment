fc31b547ea7113d175d8c155a41d7da114921aed
ngocthieuff <ptnngoc22@gmail.com>
2022-11-23 16:49:54 +0700

    replace fragment

--- app/src/main/java/com/nt/fragment/MainActivity.kt
+++ app/src/main/java/com/nt/fragment/MainActivity.kt
@@ -2,10 +2,31 @@ package com.nt.fragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val newsBtn = findViewById<Button>(R.id.btnNews)
+        val scienceBtn = findViewById<Button>(R.id.btnScience)
+        val sportsBtn = findViewById<Button>(R.id.btnSports)
+
+        newsBtn.setOnClickListener {
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.fragmentContainerView, NewsFragment())
+            transaction.commit()
+        }
+        scienceBtn.setOnClickListener {
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.fragmentContainerView, ScienceFragment())
+            transaction.commit()
+        }
+        sportsBtn.setOnClickListener {
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.fragmentContainerView, SportsFragment())
+            transaction.commit()
+        }
     }
 }
